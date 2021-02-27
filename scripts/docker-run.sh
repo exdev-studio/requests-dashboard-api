98b2bcc93eb1c0029a2cf6023635eeb6a381d538
gravityperson <gravityemail06@gmail.com>
2021-02-28 00:29:15 +0300

    Docker-compose support
    + default env config
    + docker-compose deployment
    
    ~ run docker in detached mode

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,1 +1,1 @@
-docker run --rm --name requests-dashboard-api -it --env APP_LOG_LEVEL=debug --env=APP_BIND_ADDR=:8080 -p 8080:8080 exdev-studio/requests-dashboard-api:latest
+docker run --rm --name requests-dashboard-api -d -it --env APP_LOG_LEVEL=debug --env=APP_BIND_ADDR=:8080 -p 8080:8080 exdev-studio/requests-dashboard-api:latest
